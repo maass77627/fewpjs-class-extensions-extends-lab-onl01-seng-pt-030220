90f2b0bb19791e068d56ce117e475331edc7aeee
kelly r maass <kmaass77627@gmail.com>
2020-10-17 14:48:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+class polygon(array) {
+
+  get countSides(array) {
+    array.length
+  }
+
+  get perimeter() {
+
+  }
+
+}
+
+class triangle extends polygon() {
+get isValid() {
+
+}
+}
